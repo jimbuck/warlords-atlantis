9be21183c1e9a533d595c40e0235be031333f3b1
Jim Buck <jim@jimmyboh.com>
2013-03-19 00:48:40 -0400

    Added art and initial code.
    Created two tiles (wood and iron bars). Created basic shark with title.
    Basic code loads the images then goes to main menu.

---
+++ game.js
@@ -0,0 +1,52 @@
+﻿
+
+
+$(function () {
+	Crafty.init(800, 640);
+
+	//the loading screen that will display while our assets load
+	Crafty.scene("loading", function () {
+		//load takes an array of assets and a callback when complete
+		Crafty.load(["title.png"], function () {
+			Crafty.scene("main"); //when everything is loaded, run the main scene
+		});
+
+		// Black background with some loading text
+		Crafty.background("#000");
+		Crafty.e("2D, DOM, Text").attr({
+			w : 100,
+			h : 20,
+			x : 150,
+			y : 120
+		})
+		.text("Loading...")
+		.css({
+			'text-align' : 'center',
+			'color': '#fff'
+		});
+	});
+	
+	Crafty.scene("main", function(){
+		Crafty.audio.add("theme", "audio/theme.mp3");
+		
+		//Crafty.audio.play("theme", -1, 0.5);
+		
+		// Blue background...
+		Crafty.background("#01c");
+		Crafty.e("2D, DOM, Text").attr({
+			w : 100,
+			h : 20,
+			x : 150,
+			y : 120
+		})
+		.text("Playing...")
+		.css({
+			"text-align" : "center",
+			'color': '#fff'
+		});	
+	});
+
+	// Automatically play the loading scene
+	Crafty.scene("loading");
+
+});
